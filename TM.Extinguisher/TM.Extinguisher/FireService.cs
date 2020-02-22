a77ed54f127261755f4e1608ac052cdb55c9649e
Andrei Morozov <andrei.morozov@tapmango.com>
2020-02-22 14:42:52 -0500

    removed answer from solution

--- TM.Extinguisher/TM.Extinguisher/FireService.cs
+++ TM.Extinguisher/TM.Extinguisher/FireService.cs
@@ -52,25 +52,7 @@ namespace TM.Extinguisher
         //          If FireLevel = None make sure to stop watering                       
         public void UpdateWateringStatusReports()
         {
-            var reports = _fireRepo.GetReports();
-            var currentlyExtinguishing = reports.Where(r => r.Watering).FirstOrDefault();
-
-            if (currentlyExtinguishing == null) return;
-
-            var wateringDuration = (DateTime.Now - currentlyExtinguishing.LastUpdated).TotalSeconds; 
-
-            if (wateringDuration >= 5)
-            {
-                currentlyExtinguishing.FireLevel = DecreaseFireLevel(currentlyExtinguishing.FireLevel);
-                currentlyExtinguishing.LastUpdated = DateTime.Now;
-
-                if (currentlyExtinguishing.FireLevel == FireLevel.NONE)
-                {
-                    currentlyExtinguishing.Watering = false;
-                }
-
-                UpdateFireReport(currentlyExtinguishing);
-            }
+          
         }
 
 
@@ -78,20 +60,7 @@ namespace TM.Extinguisher
         public void ExtinguishFireReport(int Id)
         {
 
-            //check if anything else being watered
-            var reports = _fireRepo.GetReports();
-            var currentlyExtinguishing = reports.Where(r => r.Watering).FirstOrDefault();
-            var fireExtinguishOrder = reports.FirstOrDefault(r => r.Id == Id);
-
-            // do we need to stop previous watering?
-            if (currentlyExtinguishing != null && currentlyExtinguishing.Id != fireExtinguishOrder.Id) {
-                currentlyExtinguishing.Watering = false;
-                UpdateFireReport(currentlyExtinguishing);
-            }
-
-            fireExtinguishOrder.Watering = true;
-            fireExtinguishOrder.LastUpdated = DateTime.Now;
-            UpdateFireReport(fireExtinguishOrder);
+          
         }
 
     }
